ca4fd18fbe191fc499e8ab159740bba44ecb8d63
aleksey.shamanov@zyfra.com <aleksey.shamanov@zyfra.com>
2021-01-29 12:14:59 +0500

    Задача 1.8.1

---
+++ C1/T181/cats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Работа с классом Cat в рамках задания 1.8.1 """
+
+import cat
+
+sam = cat.Cat('m', 2, 'сэм')
+baron = cat.Cat('m', 2, 'барон')
+print(sam.get_info())
+print(baron.get_info())
