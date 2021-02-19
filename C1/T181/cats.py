3e0e8df46267594000230375a9bc5238a6a15088
aleksey.shamanov@zyfra.com <aleksey.shamanov@zyfra.com>
2021-02-19 13:14:46 +0500

    Ветка для работы с крестиками-ноликами

--- C1/T181/cats.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""Работа с классом Cat в рамках задания 1.8.1 """
-
-import cat
-
-cats = [cat.Cat('m', 2, 'сэм'), cat.Cat('m', 2, 'барон')]
-for i in cats:
-    print(i.get_info())
