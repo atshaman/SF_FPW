e233ed9b1c98774b417b647f5fa4f5699cdc847e
aleksey.shamanov@zyfra.com <aleksey.shamanov@zyfra.com>
2021-02-19 12:39:49 +0500

    Работающая версия морского боя

--- C1/T191/areas.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""Работа с классоми геометрических фигур в рамках задания 1.9.1 """
-import figures
-
-myfigures = [figures.Rectangle(5, 10), figures.Square(15), figures.Circle(8)]
-for figure in myfigures:
-    print(figure.get_area())
