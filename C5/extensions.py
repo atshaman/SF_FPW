cdb48f23bd26ca26941cc93c95409e60ad7a9550
aleksey.shamanov@zyfra.com <aleksey.shamanov@zyfra.com>
2021-03-01 18:44:35 +0500

    Реализован класс для запроса курсов\подсчета стоимости конвертации с кэшированием.

--- C5/extensions.py
+++ C5/extensions.py
@@ -1,10 +1,52 @@
 # -*- coding: utf-8 -*-
 
-"""Определение классов для проекта 'телеграм-бот'
-Поддерживаемые команды /help /start
-формат запроса: имя валюты цену которой он хочет узнать> <имя валюты в которой надо узнать цену первой валюты> <количество первой валюты
-"""
+"""Определение классов для проекта 'телеграм-бот' """
 
 import requests
-import json
-import telebot
+import datetime
+
+
+class ApiException(Exception):
+    pass
+
+
+class GetCurrency:
+    def __init__(self, cache=None):
+        self.url = 'https://api.exchangeratesapi.io'
+        if cache:
+            self.cache = datetime.timedelta(seconds=cache)
+            self.refresh()
+        else:
+            self.cache = None
+
+    def refresh(self):
+        self.refresh = datetime.datetime.now()
+        try:
+            self.cvalues = requests.get(self.url + '/latest').json()['rates']
+        except Exception:
+            raise ApiException('Ошибка получения данных о курсе валют по сети!')
+        # Явным образом добавляем базу рассчета
+        self.cvalues['EUR'] = 1
+
+    def get_price_cache(self, base: str, quote: str, amount: int) -> float:
+        if (self.cache is None) or (datetime.datetime.now() - self.refresh > self.cache):
+            self.refresh()
+        if self.cvalues.get(base.capitalize(), None) is None:
+            raise ApiException(f'Базовая валюта {base} отсутствует в списке доступных для конвертации!')
+        if self.cvalues.get(quote.capitalize(), None) is None:
+            raise ApiException(f'Результирующая валюта {quote} отсутствует в списке доступных для конвертации!')
+        if amount <= 0:
+            raise ApiException(f'Количество обмениваемой валюты должно быть целым положительным числом > 0!')
+        base = self.cvalues.get(base)
+        quote = self.cvalues.get(quote)
+        return amount / base * quote
+
+    @staticmethod
+    def get_price(base, quote, amount):
+        pass
+
+
+if __name__ == '__main__':
+    # Unit-тест на минималках
+    exchange = GetCurrency(cache=None)
+    print(exchange.get_price_cache('EUR', 'RUB', 100))
